a03367d842df087692f485282f71732db12a07c6
abellimz <abellim1309@gmail.com>
2018-03-16 16:21:28 +0800

    add feature classifiers and keras NN implementation

---
+++ classifier/keras_classifier.py
@@ -0,0 +1,75 @@
+import os
+import abc
+
+import coremltools
+from keras.models import load_model
+
+from common import constants, utility
+from classifier.classifier import Classifier
+from common.config import MLP_KERAS_INPUT_NAME, MLP_KERAS_OUTPUT_NAME
+
+
+class KerasClassifier(Classifier, metaclass=abc.ABCMeta):
+    def __init__(self):
+        self.labels2Idx = None
+        self.labels = None
+        self.model = None
+
+    def new_model(self, class_labels, feature_shape):
+        self.labels2Idx = utility.list_to_dict(class_labels)
+        self.labels = class_labels
+
+    def save_model(self, model_dir, model_basename):
+        """
+        Saves trained sklearn model.
+        Saves in pickle and coreml formats.
+        :param model_dir Directory to be used for saving models.
+        :param model_basename Base name of output model. File extension
+            is appended to this basename in the overall filename.
+        """
+        if self.model is None:
+            raise Exception("Classifier model is not created yet")
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+
+        # Save labels file
+        labels_path = os.path.join(model_dir, constants.FILENAME_LABELS)
+        self._save_labels_file(labels_path)
+
+        # Save to pickle model file
+        keras_model_path = os.path.join(
+            model_dir, model_basename + constants.EXTENSION_H5)
+        self.model.save(keras_model_path)
+
+        # Save to coreml model file
+        coreml_model_path = os.path.join(
+            model_dir, model_basename + constants.EXTENSION_COREML)
+        coreml_model = coremltools.converters.keras\
+            .convert(self.model, MLP_KERAS_INPUT_NAME, MLP_KERAS_OUTPUT_NAME,
+                     class_labels=labels_path)
+        coreml_model.save(coreml_model_path)
+
+    def load_model(self, model_dir, model_basename):
+        """
+        Loads model into object
+        :param model_path: file name for model to be loaded.
+        """
+        labels_path = os.path.join(model_dir, constants.FILENAME_LABELS)
+        self._load_labels_file(labels_path)
+
+        keras_model_path = os.path.join(
+            model_dir, model_basename + constants.EXTENSION_H5)
+        self.model = load_model(keras_model_path)
+
+    def _save_labels_file(self, filename):
+        with open(filename, "w") as f:
+            for label in self.labels:
+                f.write(label + '\n')
+
+    def _load_labels_file(self, filename):
+        if not os.path.exists(filename):
+            raise Exception("No labels file found in models directory")
+
+        with open(filename, "r") as f:
+            self.labels = [x.strip("\n") for x in f.readlines()]
+            self.labels2Idx = utility.list_to_dict(self.labels)
---
+++ classifier/mlp_keras_classifier.py
@@ -0,0 +1,73 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+from common.config import (
+    MLP_CLASSIFIER_NUM_EPOCHS,
+    MLP_CLASSIFIER_BATCH_SIZE,
+    MLP_CLASSIFIER_HIDDEN_SIZE
+)
+from classifier.keras_classifier import KerasClassifier
+
+class MLPKerasClassifier(KerasClassifier):
+
+    def new_model(self, class_labels, feature_shape):
+        super().new_model(class_labels, feature_shape)
+        self.model = Sequential()
+        self.model.add(
+            Dense(MLP_CLASSIFIER_HIDDEN_SIZE,
+                  activation="relu",
+                  input_shape=feature_shape,
+                  name="hidden_layer"))
+        self.model.add(
+            Dense(len(self.labels),
+                  activation="softmax",
+                  name="output_layer"))
+        self.model.compile(optimizer="rmsprop",
+                           loss="sparse_categorical_crossentropy",
+                           metrics=["accuracy"])
+
+    def train(self, features, labels):
+        """
+        Trains NN model with given list of training features.
+        Labels should also be supplied such that they correspond to their
+        respective training feature..
+        :param features: list of training features
+        :param labels: list of labels corresponding to each training features.
+        """
+        if self.model is None:
+            raise Exception("No model created or loaded yet")
+
+        encoded_labels = list(map(lambda x: self.labels2Idx[x], labels))
+        encoded_labels = np.asarray(encoded_labels)
+        features = np.asarray(features)
+        self.model.fit(features, encoded_labels,
+                       batch_size = MLP_CLASSIFIER_BATCH_SIZE,
+                       epochs = MLP_CLASSIFIER_NUM_EPOCHS,
+                       validation_split = 0.1)
+
+    def infer(self, features):
+        """
+        Performs inference using model on given list of features.
+        :param features: list of features in numpy
+        :return: list of list of confidence values in shape (n_examples, n_classes)
+            For each confidence value list, every element at ith index corresponds to
+            the confidence value for the ith class in the classes property.
+        """
+        if self.model is None:
+            raise Exception("No model created or loaded yet")
+
+        features = np.asarray(features)
+        return self.model.predict(features, MLP_CLASSIFIER_BATCH_SIZE).tolist()
+
+    def get_classes(self):
+        """
+        Returns the list of classes used by this classifier.
+        The order of classes corresponds to that in confidence scores from
+        inference. This list is also in the same order as class_labels if new_model()
+        method is used to generate the model. Otherwise, it is in the same order as
+        defined in loaded model.
+        """
+        if self.labels is None:
+            raise Exception("Classes are only available after model creation/load")
+        return self.labels
